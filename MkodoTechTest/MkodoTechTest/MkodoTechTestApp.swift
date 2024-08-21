a20d5339fcf35cd32a6bd99ff4aaafdc77dad8fa
Bashir <bashir@fsbtech.com>
2024-08-21 08:40:19 +0100

    Initial Commit

---
+++ MkodoTechTest/MkodoTechTest/MkodoTechTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MkodoTechTestApp.swift
+//  MkodoTechTest
+//
+//  Created by Bashir Sentongo on 21/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MkodoTechTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+
+        }
+    }
+}
